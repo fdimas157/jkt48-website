59f2191ba0b14525fe48821e956fef1b162a94cb
Dimas Firmansyah <fdimas157@gmail.com>
2024-03-30 21:50:33 +0700

    fix: add more member in member page

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -203,6 +203,94 @@ export default function Member() {
     },
   ];
 
+  const newGenerations = [
+    {
+      id: 1,
+      name: "Abigail Rachel",
+      image: "/src/assets/kabesha/aralie.jpg",
+    },
+    {
+      id: 2,
+      name: "Adeline Wijaya",
+      image: "/src/assets/kabesha/delynn.jpg",
+    },
+    {
+      id: 3,
+      name: "Aisa Maharani",
+      image: "/src/assets/kabesha/shasa.jpg",
+    },
+    {
+      id: 4,
+      name: "Aurhel Alana",
+      image: "/src/assets/kabesha/lana.jpg",
+    },
+    {
+      id: 5,
+      name: "Catherina Vallencia",
+      image: "/src/assets/kabesha/erine.jpg",
+    },
+    {
+      id: 6,
+      name: "Fritzy Rosmerian",
+      image: "/src/assets/kabesha/fritzy.jpg",
+    },
+    {
+      id: 7,
+      name: "Hillary Abigail",
+      image: "/src/assets/kabesha/lily.jpg",
+    },
+    {
+      id: 8,
+      name: "Jazzlyn Trisha",
+      image: "/src/assets/kabesha/trisha.jpg",
+    },
+    {
+      id: 9,
+      name: "Letycia Moreen",
+      image: "/src/assets/kabesha/moreen.jpg",
+    },
+    {
+      id: 10,
+      name: "Michelle Levia",
+      image: "/src/assets/kabesha/levi.jpg",
+    },
+    {
+      id: 11,
+      name: "Nayla Suji",
+      image: "/src/assets/kabesha/nayla.jpg",
+    },
+    {
+      id: 12,
+      name: "Nina Tutachia",
+      image: "/src/assets/kabesha/nachia.jpg",
+    },
+    {
+      id: 13,
+      name: "Oline Manuel",
+      image: "/src/assets/kabesha/oline.jpg",
+    },
+    {
+      id: 14,
+      name: "Regina Wilian",
+      image: "/src/assets/kabesha/regie.jpg",
+    },
+    {
+      id: 15,
+      name: "Ribka Budiman",
+      image: "/src/assets/kabesha/ribka.jpg",
+    },
+    {
+      id: 16,
+      name: "Shabilqis Naila",
+      image: "/src/assets/kabesha/nala.jpg",
+    },
+    {
+      id: 17,
+      name: "Victoria Kimberly",
+      image: "/src/assets/kabesha/kimmy.jpg",
+    },
+  ];
+
   return (
     <>
       <main>
@@ -241,6 +329,22 @@ export default function Member() {
                 {trainees.map((m) => (
                   <div
                     className="w-32 rounded-lg border-4 border-red-600 cursor-pointer"
+                    key={m.id}
+                  >
+                    <img src={m.image} alt={m.name} className="rounded-lg" />
+                    <div className="font-outfit text-red-600 h-8 flex justify-center items-center font-semibold text-xs">
+                      {m.name}
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="flex justify-start items-center h-16 px-6 bg-red-600 w-full text-white font-abel font-bold rounded-lg text-2xl">
+                JKT48 NEW GENERATION
+              </div>
+              <div className="flex flex-wrap justify-center gap-6">
+                {newGenerations.map((m) => (
+                  <div
+                    className="w-36 rounded-lg border-4 border-red-600 cursor-pointer"
                     key={m.id}
                   >
                     <img src={m.image} alt={m.name} className="rounded-lg" />
